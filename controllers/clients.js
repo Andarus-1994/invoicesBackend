6f986ed339b09b8b80cae638f2e45ea7ef6930b7
Andrei <andrei_king1@yahoo.com>
2023-12-23 15:05:43 +0200

    Update Client API

--- controllers/clients.js
+++ controllers/clients.js
@@ -25,4 +25,18 @@ const create = async (req, res) => {
   }
 }
 
-module.exports = { getAll, create }
+const update = async (req, res) => {
+  const { name, address, company_name, company_address, id } = req.body
+  const insertDataQuery = "UPDATE clients SET name = ?, address = ?, company_name = ?, company_address = ? WHERE id = ?"
+  const values = [name, address, company_name, company_address, id]
+
+  try {
+    const results = await executeQuery(insertDataQuery, values)
+    res.json({ success: true, results })
+  } catch (error) {
+    console.error("Error inserting data:", error)
+    res.status(500).json({ success: false, error: "Internal Server Error" })
+  }
+}
+
+module.exports = { getAll, create, update }
--- routes/clients.js
+++ routes/clients.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
-const { executeQuery } = require("../services/database")
-const { getAll, create } = require("../controllers/clients")
+const { getAll, create, update } = require("../controllers/clients")
 
 module.exports = () => {
   router.get("/getAll", getAll)
 
   router.post("/create", create)
+
+  router.post("/update", update)
 
   return router
 }
