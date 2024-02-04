c1ad859823b0b498e13ec64e8ff06385d1f60313
Andrei <andrei_king1@yahoo.com>
2024-02-04 13:48:36 +0200

    Keep alive function run

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const invoicesRoutes = require("./routes/invoices")()
 const clientsRoutes = require("./routes/clients")()
 const cors = require("cors")
 
+const startKeepAlive = require("./keep-alive")
+
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/invoices", invoicesRoutes)
 app.use("/clients", clientsRoutes)
+
+startKeepAlive()
 
 app.listen(5000, () => {
   console.log("Server started on port 5000")
