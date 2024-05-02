9d6d60f9540c917c5b644df6e5b36514b7ffb4d9
Andrei <andrei_king1@yahoo.com>
2024-05-02 18:00:09 +0300

    Added getById + update invoice

--- controllers/invoices.js
+++ controllers/invoices.js
@@ -60,13 +60,14 @@ const filter = async (req, res) => {
   const values = []
   const period = req.body.period
 
-  if (period !== "") {
+  if (period && period !== "") {
     const fromDate = new Date()
     fromDate.setDate(fromDate.getDate() - period)
 
     selectDataQuery += " WHERE issue_date >= $1 AND issue_date <= NOW() "
     values.push(fromDate.toISOString().split("T")[0])
   }
+
   // limit to 20 invoices and order by issue_date
   selectDataQuery += "ORDER BY issue_date DESC LIMIT 20"
 
@@ -80,7 +81,26 @@ const filter = async (req, res) => {
 
     res.json({ success: true, results })
   } catch (error) {
-    console.error("Error inserting data:", error)
+    console.error("Error filtering data:", error)
+    res.status(500).json({ success: false, error: "Internal Server Error" })
+  }
+}
+
+const getById = async (req, res) => {
+  // created a small timeout
+
+  if (!req.body.id) return res.json({ error: true, message: "No ID was selected." })
+
+  let selectDataQuery = "SELECT * FROM invoices WHERE id = $1"
+  const values = []
+  values.push(req.body.id)
+
+  try {
+    const results = await executeQueryPsql(selectDataQuery, values)
+    if (results.length) res.json({ success: true, data: results[0] })
+    else res.json({ success: false, error: "Invoice Not Found" })
+  } catch (error) {
+    console.error("Error getting data by ID:", error)
     res.status(500).json({ success: false, error: "Internal Server Error" })
   }
 }
@@ -96,14 +116,42 @@ const remove = async (req, res) => {
 
     res.json({ success: true, results })
   } catch (error) {
-    console.error("Error inserting data:", error)
+    console.error("Error removing data:", error)
+    res.status(500).json({ success: false, error: "Internal Server Error" })
+  }
+}
+
+const update = async (req, res) => {
+  if (!req.body.id) return res.json({ error: true, message: "No ID was selected." })
+
+  const { id, ...updateValues } = req.body
+
+  const mappedValues = Object.entries(updateValues)
+    .filter(([_, value]) => value !== undefined)
+    .map(([key, value]) => (key === "due_date" || key === "issue_date" ? [key, parseFrontendDate(value)] : [key, value]))
+
+  const columnsToUpdate = mappedValues.map(([key], index) => `${key} = $${index + 1}`).join(", ")
+
+  const updateDataQuery = `UPDATE invoices SET ${columnsToUpdate} WHERE id = $${mappedValues.length + 1}`
+
+  try {
+    const values = mappedValues.map(([_, value]) => value)
+    values.push(id)
+
+    await executeQueryPsql(updateDataQuery, values)
+
+    res.json({ success: true })
+  } catch (error) {
+    console.error("Couldn't be updated ", error)
     res.status(500).json({ success: false, error: "Internal Server Error" })
   }
 }
 
 module.exports = {
   getAll,
+  getById,
   create,
   filter,
+  update,
   remove,
 }
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const router = express.Router()
 
-const { getAll, create, filter, remove } = require("../controllers/invoices")
+const { getAll, getById, create, filter, update, remove } = require("../controllers/invoices")
 
 module.exports = () => {
   router.post("/create", create)
 
   router.get("/getAll", getAll)
 
+  router.post("/getById", getById)
+
   router.post("/filter", filter)
+
+  router.post("/update", update)
 
   router.post("/remove", remove)
 
